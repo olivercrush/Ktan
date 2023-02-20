a6fd63a0b3e7c503b537d56d951e2936764b5c44
olivercrush <olivier.beguinti@gmail.com>
2023-02-20 16:15:34 -0500

    fixed grid representation and hex to location mapping

--- src/test/java/com/orinine/ktan/board/utils/HexToLocationsMapperTest.java
+++ src/test/java/com/orinine/ktan/board/utils/HexToLocationsMapperTest.java
@@ -1,7 +1,5 @@
 package com.orinine.ktan.board.utils;
 
-import com.orinine.ktan.board.utils.HexToLocationsMapper;
-import com.orinine.ktan.board.utils.Point;
 import org.junit.jupiter.api.Test;
 
 public class HexToLocationsMapperTest {
@@ -11,22 +9,34 @@ public class HexToLocationsMapperTest {
         var locations = HexToLocationsMapper.map(new Point(0, 0));
 
         assert(locations.get(0).equals(new Point(0, 0)));
-        assert(locations.get(1).equals(new Point(1, 0)));
-        assert(locations.get(2).equals(new Point(0, 1)));
-        assert(locations.get(3).equals(new Point(1, 1)));
-        assert(locations.get(4).equals(new Point(0, 2)));
-        assert(locations.get(5).equals(new Point(1, 2)));
+        assert(locations.get(1).equals(new Point(0, 1)));
+        assert(locations.get(2).equals(new Point(1, 1)));
+        assert(locations.get(3).equals(new Point(0, 2)));
+        assert(locations.get(4).equals(new Point(1, 2)));
+        assert(locations.get(5).equals(new Point(0, 3)));
     }
 
     @Test
     public void testOneZeroHexToPoints() {
         var locations = HexToLocationsMapper.map(new Point(1, 0));
 
-        assert(locations.get(0).equals(new Point(1, 1)));
+        assert(locations.get(0).equals(new Point(1, 0)));
+        assert(locations.get(1).equals(new Point(1, 1)));
+        assert(locations.get(2).equals(new Point(2, 1)));
+        assert(locations.get(3).equals(new Point(1, 2)));
+        assert(locations.get(4).equals(new Point(2, 2)));
+        assert(locations.get(5).equals(new Point(1, 3)));
+    }
+
+    @Test
+    public void testTwoZeroHexToPoints() {
+        var locations = HexToLocationsMapper.map(new Point(2, 0));
+
+        assert(locations.get(0).equals(new Point(2, 0)));
         assert(locations.get(1).equals(new Point(2, 1)));
-        assert(locations.get(2).equals(new Point(1, 2)));
+        assert(locations.get(2).equals(new Point(3, 1)));
         assert(locations.get(3).equals(new Point(2, 2)));
-        assert(locations.get(4).equals(new Point(1, 3)));
+        assert(locations.get(4).equals(new Point(3, 2)));
         assert(locations.get(5).equals(new Point(2, 3)));
     }
 
@@ -34,12 +44,12 @@ public class HexToLocationsMapperTest {
     public void testZeroOneHexToPoints() {
         var locations = HexToLocationsMapper.map(new Point(0, 1));
 
-        assert(locations.get(0).equals(new Point(0, 2)));
-        assert(locations.get(1).equals(new Point(1, 2)));
-        assert(locations.get(2).equals(new Point(0, 3)));
-        assert(locations.get(3).equals(new Point(1, 3)));
-        assert(locations.get(4).equals(new Point(0, 4)));
-        assert(locations.get(5).equals(new Point(1, 4)));
+        assert(locations.get(0).equals(new Point(0, 3)));
+        assert(locations.get(1).equals(new Point(0, 4)));
+        assert(locations.get(2).equals(new Point(1, 4)));
+        assert(locations.get(3).equals(new Point(0, 5)));
+        assert(locations.get(4).equals(new Point(1, 5)));
+        assert(locations.get(5).equals(new Point(0, 6)));
     }
 
     @Test
@@ -47,10 +57,58 @@ public class HexToLocationsMapperTest {
         var locations = HexToLocationsMapper.map(new Point(1, 1));
 
         assert(locations.get(0).equals(new Point(1, 3)));
-        assert(locations.get(1).equals(new Point(2, 3)));
-        assert(locations.get(2).equals(new Point(1, 4)));
-        assert(locations.get(3).equals(new Point(2, 4)));
-        assert(locations.get(4).equals(new Point(1, 5)));
-        assert(locations.get(5).equals(new Point(2, 5)));
+        assert(locations.get(1).equals(new Point(1, 4)));
+        assert(locations.get(2).equals(new Point(2, 4)));
+        assert(locations.get(3).equals(new Point(1, 5)));
+        assert(locations.get(4).equals(new Point(2, 5)));
+        assert(locations.get(5).equals(new Point(1, 6)));
+    }
+
+    @Test
+    public void testTwoOneHexToPoints() {
+        var locations = HexToLocationsMapper.map(new Point(2, 1));
+
+        assert(locations.get(0).equals(new Point(2, 3)));
+        assert(locations.get(1).equals(new Point(2, 4)));
+        assert(locations.get(2).equals(new Point(3, 4)));
+        assert(locations.get(3).equals(new Point(2, 5)));
+        assert(locations.get(4).equals(new Point(3, 5)));
+        assert(locations.get(5).equals(new Point(2, 6)));
+    }
+
+    @Test
+    public void testZeroTwoHexToPoints() {
+        var locations = HexToLocationsMapper.map(new Point(0, 2));
+
+        assert(locations.get(0).equals(new Point(0, 6)));
+        assert(locations.get(1).equals(new Point(0, 7)));
+        assert(locations.get(2).equals(new Point(1, 7)));
+        assert(locations.get(3).equals(new Point(0, 8)));
+        assert(locations.get(4).equals(new Point(1, 8)));
+        assert(locations.get(5).equals(new Point(0, 9)));
+    }
+
+    @Test
+    public void testOneTwoHexToPoints() {
+        var locations = HexToLocationsMapper.map(new Point(1, 2));
+        // !
+        assert(locations.get(0).equals(new Point(1, 6)));
+        assert(locations.get(1).equals(new Point(1, 7)));
+        assert(locations.get(2).equals(new Point(2, 7)));
+        assert(locations.get(3).equals(new Point(1, 8)));
+        assert(locations.get(4).equals(new Point(2, 8)));
+        assert(locations.get(5).equals(new Point(1, 9)));
+    }
+
+    @Test
+    public void testTwoTwoHexToPoints() {
+        var locations = HexToLocationsMapper.map(new Point(2, 2));
+
+        assert(locations.get(0).equals(new Point(2, 6)));
+        assert(locations.get(1).equals(new Point(2, 7)));
+        assert(locations.get(2).equals(new Point(3, 7)));
+        assert(locations.get(3).equals(new Point(2, 8)));
+        assert(locations.get(4).equals(new Point(3, 8)));
+        assert(locations.get(5).equals(new Point(2, 9)));
     }
 }
