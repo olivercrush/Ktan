1f5c292cd29580e772ecfc05ab4429c3e0076b27
olivercrush <olivier.beguinti@gmail.com>
2023-04-15 13:03:03 -0400

    villages, cities and roads now functional with player colors

--- src/test/java/com/orinine/ktan/KtanApplicationTests.java
+++ src/test/java/com/orinine/ktan/KtanApplicationTests.java
@@ -6,6 +6,8 @@ import com.orinine.ktan.state.board.hexgrid.ClassicHexGridGenerator;
 import com.orinine.ktan.state.board.model.Building;
 import com.orinine.ktan.state.board.model.Hex;
 import com.orinine.ktan.state.board.model.Location;
+import com.orinine.ktan.state.board.model.Road;
+import com.orinine.ktan.state.board.util.Point;
 import com.orinine.ktan.state.debug.DebugStateObserver;
 import com.orinine.ktan.state.player.PlayerColor;
 import org.junit.jupiter.api.Test;
@@ -32,7 +34,24 @@ class KtanApplicationTests {
                 .setHexGrid(hexGrid)
                 .build();
 
-        board.getLocationGrid()[5][5] = new Location(PlayerColor.RED, Building.VILLAGE, null);
+        board.getLocationGrid()[5][3] = new Location(PlayerColor.BLUE, Building.VILLAGE, null);
+        board.getLocationGrid()[3][2] = new Location(PlayerColor.BLUE, Building.CITY, null);
+        board.getLocationGrid()[3][4] = new Location(PlayerColor.RED, Building.CITY, null);
+        board.getLocationGrid()[4][0] = new Location(PlayerColor.WHITE, Building.VILLAGE, null);
+        board.getLocationGrid()[6][1] = new Location(PlayerColor.WHITE, Building.VILLAGE, null);
+        board.getLocationGrid()[7][2] = new Location(PlayerColor.YELLOW, Building.CITY, null);
+        board.getLocationGrid()[9][3] = new Location(PlayerColor.YELLOW, Building.VILLAGE, null);
+        board.getLocationGrid()[11][3] = new Location(PlayerColor.YELLOW, Building.CITY, null);
+        
+        board.getRoads().add(new Road(PlayerColor.BLUE, List.of(new Point(2, 3), new Point(2, 4))));
+        board.getRoads().add(new Road(PlayerColor.BLUE, List.of(new Point(2, 4), new Point(3, 5))));
+        board.getRoads().add(new Road(PlayerColor.RED, List.of(new Point(4, 3), new Point(4, 4))));
+        board.getRoads().add(new Road(PlayerColor.WHITE, List.of(new Point(0, 4), new Point(1, 5))));
+        board.getRoads().add(new Road(PlayerColor.WHITE, List.of(new Point(1, 5), new Point(1, 6))));
+        board.getRoads().add(new Road(PlayerColor.YELLOW, List.of(new Point(2, 7), new Point(2, 8))));
+        board.getRoads().add(new Road(PlayerColor.YELLOW, List.of(new Point(2, 8), new Point(3, 9))));
+        board.getRoads().add(new Road(PlayerColor.YELLOW, List.of(new Point(3, 9), new Point(3, 10))));
+        board.getRoads().add(new Road(PlayerColor.YELLOW, List.of(new Point(3, 10), new Point(3, 11))));
 
         state.updateBoard(board);
         Thread.sleep(100000);
--- visualizer/socket_debug_visualizer.py
+++ visualizer/socket_debug_visualizer.py
@@ -15,15 +15,18 @@ GLOBAL_Y_OFFSET = 100
 HOST = "127.0.0.1"
 PORT = 65500
 
-BLACK = 0, 0, 0
-RED = 255, 0, 0
+BRIGHT_RED = 255, 0, 0
+BRIGHT_YELLOW = 255, 255, 0
+YELLOW = 128, 128, 0
+BLUE = 0, 0, 255
+GREEN = 0, 128, 0
+ORANGE = 128, 64, 0
+DARK_YELLOW = 64, 64, 0
+DARK_GREEN = 0, 64, 0
 GREY = 128, 128, 128
+BLACK = 0, 0, 0
 WHITE = 255, 255, 255
-YELLOW = 255, 255, 0
-LIGHT_YELLOW = 128, 128, 0
-ORANGE = 255, 128, 0
-GREEN = 0, 255, 0
-LIGHT_GREEN = 0, 128, 0
+
 
 board_json = None
 board_updated = False
@@ -56,7 +59,10 @@ def start_visualizer():
 
             for y in range(len(board_json['locationGrid'])):
                 for x in range(len(board_json['locationGrid'][y])):
-                    draw_location(screen, board_json['locationGrid'][y][x], x, y)
+                    draw_location(screen, board_json['locationGrid'][y][x]['building'], board_json['locationGrid'][y][x]['color'], x, y)
+
+            for r in board_json['roads']:
+                draw_road(screen, r["playerColor"], r["points"][0], r["points"][1])
 
             pygame.display.flip()
 
@@ -80,7 +86,24 @@ def socket_thread(name):
                     board_updated = True
                     conn.sendall(data)
 
-def draw_location(surface, type, x, y):
+def draw_road(surface, color, start, end):
+    point1 = get_display_location_coordinates(int(start['x']), int(start['y']))
+    point2 = get_display_location_coordinates(int(end['x']), int(end['y']))
+    pygame.draw.line(surface, get_color_from_player_color(color), point1, point2, 3)
+
+def draw_location(surface, type, color, x, y):
+    if type == "VILLAGE":
+        pygame.draw.circle(surface, get_color_from_player_color(color), get_display_location_coordinates(x, y), 5)
+    elif type == "CITY":
+        pygame.draw.circle(surface, get_color_from_player_color(color), get_display_location_coordinates(x, y), 5)
+        pygame.draw.circle(surface, get_color_from_player_color(color), get_display_location_coordinates(x, y), 10, 3)
+
+    #display_x, display_y = get_display_location_coordinates(x, y)
+    #font = pygame.font.SysFont(None, 24)
+    #img = font.render("(" + str(x) + ";" + str(y) + ")", True, RED)
+    #surface.blit(img, (display_x + 5, display_y - 20))
+
+def get_display_location_coordinates(x, y):
     x_offset = 0
     y_offset = 0
     if math.ceil(y / 2) % 2 == 0:
@@ -96,12 +119,17 @@ def draw_location(surface, type, x, y):
     display_x = x * HEX_WIDTH + x_offset + GLOBAL_X_OFFSET
     display_y = math.floor(y / 4) * (HEX_HEIGTH + HEX_HEIGTH / 2) + y_offset + GLOBAL_Y_OFFSET
 
-    pygame.draw.circle(surface, RED, (display_x, display_y), 3)
-    font = pygame.font.SysFont(None, 24)
-    img = font.render("(" + str(x) + ";" + str(y) + ")", True, RED)
-    surface.blit(img, (display_x + 5, display_y - 20))
+    return (display_x, display_y)
 
 def draw_hexagone(surface, type, dice_target, x, y):
+    pygame.draw.polygon(surface, get_color_from_hex_type(type), get_display_hex_vertices(x, y))
+
+    if type != "DESERT":
+        font = pygame.font.SysFont(None, 24)
+        img = font.render(str(dice_target), True, BLACK)
+        surface.blit(img, get_display_hex_center(x, y))
+
+def get_display_hex_center(x, y):
     row_offset = 0
     col_offset = 0
     if y % 2 == 1:
@@ -109,6 +137,11 @@ def draw_hexagone(surface, type, dice_target, x, y):
 
     center_x = x * HEX_WIDTH + row_offset + GLOBAL_X_OFFSET + HEX_WIDTH / 2
     center_y = y * (HEX_HEIGTH * 3 / 4) + GLOBAL_Y_OFFSET + HEX_HEIGTH / 2
+
+    return (center_x, center_y)
+
+def get_display_hex_vertices(x, y):
+    center_x, center_y = get_display_hex_center(x, y)
 
     hex_vertices = (
         (center_x, center_y - HEX_HEIGTH / 2), 
@@ -118,12 +151,19 @@ def draw_hexagone(surface, type, dice_target, x, y):
         (center_x + HEX_WIDTH / 2, center_y + HEX_HEIGTH / 4), 
         (center_x + HEX_WIDTH / 2, center_y - HEX_HEIGTH / 4)
     )
-    pygame.draw.polygon(surface, get_color_from_hex_type(type), hex_vertices)
 
-    if type != "DESERT":
-        font = pygame.font.SysFont(None, 24)
-        img = font.render(str(dice_target), True, BLACK)
-        surface.blit(img, (center_x, center_y))
+    return hex_vertices
+
+def get_color_from_player_color(player_color):
+    if player_color == "RED":
+        return BRIGHT_RED
+    elif player_color == "BLUE":
+        return BLUE
+    elif player_color == "YELLOW":
+        return BRIGHT_YELLOW
+    elif player_color == "WHITE":
+        return WHITE
+    return BLACK
 
 def get_color_from_hex_type(hex_type):
     if hex_type == "FIELDS":
@@ -131,13 +171,13 @@ def get_color_from_hex_type(hex_type):
     elif hex_type == "FOREST":
         return GREEN
     elif hex_type == "PASTURE":
-        return LIGHT_GREEN
+        return DARK_GREEN
     elif hex_type == "MOUNTAINS":
         return GREY
     elif hex_type == "HILLS":
         return ORANGE
     elif hex_type == "DESERT":
-        return LIGHT_YELLOW
+        return DARK_YELLOW
 
     return BLACK
 
